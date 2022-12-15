e81df2e0020da0f210d947af052d79ddc4248efe
knnv-ar <knvv.ar@gmail.com>
2022-12-15 13:42:34 -0300

    Update: button three with JS action when clicked

---
+++ main.js
@@ -0,0 +1,10 @@
+const buttonThree = document.querySelector('.button-three');
+
+buttonThree.addEventListener('click', () => {
+  const isOpened = buttonThree.getAttribute('aria-expanded');
+  if (isOpened === 'false') {
+    buttonThree.setAttribute('aria-expanded', 'true');
+  } else {
+    buttonThree.setAttribute('aria-expanded', 'false');
+  }
+})
